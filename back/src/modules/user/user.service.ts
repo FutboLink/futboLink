194e786aedd29e32e969b411f7402753f50b377c
ConstanzaHerrera-code <conty1505@gmail.com>
2025-01-10 19:15:10 -0300

    funcionan

--- back/src/modules/user/user.service.ts
+++ back/src/modules/user/user.service.ts
@@ -1,45 +1,69 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { RegisterUserDto } from './dto/create-user.dto';
-import * as bcrypt from "bcrypt" 
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UserService {  constructor(
-  @InjectRepository(User)
-  private readonly userRepository: Repository<User>,
-) {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
 
-async register(registerUserDto: RegisterUserDto): Promise<User> {
-  const { email, password, ...otherDetails } = registerUserDto;
+  async register(registerUserDto: RegisterUserDto): Promise<User> {
+    const { email, password, ...otherDetails } = registerUserDto;
 
-  const existingUser = await this.userRepository.findOne({ where: { email } });
-  if (existingUser) {
-    throw new ConflictException('Email already in use');
+    const existingUser = await this.userRepository.findOne({ where: { email } });
+    if (existingUser) {
+      throw new ConflictException('Email already in use');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = this.userRepository.create({
+      email,
+      password: hashedPassword,
+      ...otherDetails,
+    });
+
+    return this.userRepository.save(newUser);
   }
 
-  const hashedPassword = await bcrypt.hash(password, 10);
+  async findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
 
-  const newUser = this.userRepository.create({
-    email,
-    password: hashedPassword,
-    ...otherDetails,
-  });
+  async findOne(id: string): Promise<User> {
+    const user = await this.userRepository.findOne({ where: { id } });
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+    return user;
+  }
 
-  return this.userRepository.save(newUser);
+  async remove(id: string): Promise<void> {
+    const result = await this.userRepository.delete(id);
+    if (result.affected === 0) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+  }
+
+  async updateUser(id: string, user: Partial<User>): Promise<Partial<User>> {
+    if (Object.keys(user).length === 0) {
+      throw new BadRequestException('No update values provided');
+    }
+    await this.userRepository.update(id, user);
+    const updateUser = await this.userRepository.findOneBy({ id });
+
+    if (!updateUser) {
+      throw new NotFoundException(`Usuario con id ${id} no encontrado`);
+    }
+
+    const { password, ...userNoSensitiveInfo } = updateUser;
+
+    return userNoSensitiveInfo;
+  }
 }
 
-  findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
-}

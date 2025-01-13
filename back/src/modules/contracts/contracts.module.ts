1352865748018b195630616b5e8db10c1e6a4f62
Ignacio Jordan Coria <ignaciojordan17@gmail.com>
2025-01-13 12:12:41 -0300

    final fixes contract-application para deploy

--- back/src/modules/contracts/contracts.module.ts
+++ back/src/modules/contracts/contracts.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ContractsController } from './contracts.controller';
 import { ContractService } from './contracts.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Application } from '../Applications/entities/applications.entity';
+import { Contract } from './entities/contract.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Contract, Application])],
   controllers: [ContractsController],
   providers: [ContractService],
 })

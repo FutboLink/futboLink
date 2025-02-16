690822257e0147c4b1e4cf84485dc8ab6d66d8c7
ArcadiaGroup-Dev <arcadiagroup.dev@gmail.com>
2025-02-16 18:40:32 -0300

    habilitacion vercel

--- back/src/main.ts
+++ back/src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001', 
+    origin: [
+      'http://localhost:3001', 
+      'https://futbolcareer-52favj1to-arcadias-projects-616f0f4b.vercel.app'
+    ],
     methods: 'GET,POST,PUT,DELETE',
     credentials: true,
   });

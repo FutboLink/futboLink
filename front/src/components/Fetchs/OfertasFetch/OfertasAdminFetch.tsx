e3f7502bea1e41c3ed6dca09b5eb0d10d359207e
ArcadiaGroup-Dev <arcadiagroup.dev@gmail.com>
2025-03-15 01:48:19 -0300

    Prueba produccion

--- front/src/components/Fetchs/OfertasFetch/OfertasAdminFetch.tsx
+++ front/src/components/Fetchs/OfertasFetch/OfertasAdminFetch.tsx
@@ -3,20 +3,21 @@ import {  IOfferCard } from "@/Interfaces/IOffer";
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 
-// Función para obtener jobs
-export const getOfertas =  async (): Promise<IOfferCard[]> => {
-    try {
-      const response = await fetch(`${apiUrl}/jobs`); 
-      if (!response.ok) {
-        throw new Error('Error al obtener los usuarios');
-      }
-      const jobs: IOfferCard[] = await response.json();
-      return jobs;
-    } catch (error) {
-      console.error(error);
-      return [];
+export const getOfertas = async (): Promise<IOfferCard[]> => {
+  try {
+    const response = await fetch(`${apiUrl}/jobs`);
+    if (!response.ok) {
+      const errorDetails = await response.text();  // Captura el mensaje de error detallado
+      throw new Error(`Error al obtener los trabajos: ${errorDetails}`);
     }
-  };
+    const jobs: IOfferCard[] = await response.json();
+    return jobs;
+  } catch (error) {
+    console.error("Error en getOfertas:", error);
+    return [];
+  }
+};
+
   
   export const getOfertaById = async (id: string): Promise<IOfferCard | null> => {
     const response = await fetch(`${apiUrl}/jobs/${id}`);

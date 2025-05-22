eb5e9a78fec7472bd11e0f6edbd90005332fea24
fedesuarez16 <fedesuarez16@gmail.com>
2025-05-22 02:15:49 -0300

    idk

--- front/src/helpers/helpersSubs.ts
+++ front/src/helpers/helpersSubs.ts
@@ -38,10 +38,10 @@ export function Subscription() {
       borderColor: "border-verde-claro",
       buttonColor: "bg-verde-claro",
       priceId: {
-        monthly: "price_1RP80ZGbCHvHfqXF9CqoLtnt",
+        monthly: "price_1R7MPlGbCHvHfqXFNjW8oj2k",
         yearly: "price_1R7MPlGbCHvHfqXFapD8MeOw",
       },
-      productId: "prod_SJlX3qKmAGTGw6",
+      productId: "prod_S1PExFzjXvaE7E",
     },
     {
       title: "Profesional",

be5c66ede72c2439e75793e9a2d4d79ad534573a
fedesuarez16 <fedesuarez16@gmail.com>
2025-06-06 04:40:42 -0300

    idk

--- front/src/components/home/home.tsx
+++ front/src/components/home/home.tsx
@@ -13,6 +13,7 @@ import NavbarRoles from "@/components/navbar/navbarRoles";
 import ClientsSection from "@/components/Clients/client";
 import { useEffect } from "react";
 import LanguageToggle from "@/components/LanguageToggle/LanguageToggle";
+import { useRouter } from "next/navigation";
 
 // Swiper
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -27,25 +28,33 @@ import futC from "../../../public/cursosYformaciones2.jpg";
 
 const Home = () => {
   const { role } = useContext(UserContext);
+  const router = useRouter();
 
   const images = [
     {
       src: futA,
       text: "Buscador de ofertas laborales",
+      link: "/jobs",
     },
     {
       src: futB,
       text: "Publicar oferta laboral",
+      link: "/OptionUsers",
     },
     {
       src: futC,
       text: "Cursos y Formaciones",
+      link: "/cursos",
     },
   ];
 
   useEffect(() => {
     AOS.init({ duration: 1000, once: false });
   }, []);
+
+  const handleSlideClick = (link: string) => {
+    router.push(link);
+  };
 
   return (
     <main className="bg-verde-oscuro text-white relative overflow-hidden">
@@ -68,7 +77,10 @@ const Home = () => {
         >
           {images.map((image, index) => (
             <SwiperSlide key={index}>
-              <div className="relative w-full h-full">
+              <div 
+                className="relative w-full h-full cursor-pointer" 
+                onClick={() => handleSlideClick(image.link)}
+              >
                 <Image
                   src={image.src}
                   fill
@@ -76,7 +88,7 @@ const Home = () => {
                   className="object-cover"
                   priority
                 />
-                <div className="absolute inset-0 bg-black bg-opacity-30 flex items-center justify-center">
+                <div className="absolute inset-0 bg-black bg-opacity-30 flex items-center justify-center hover:bg-opacity-50 transition-all duration-300">
                   <div className="text-white text-2xl sm:text-4xl font-bold bg-black bg-opacity-50 px-8 py-4 rounded-lg">
                     {image.text}
                   </div>

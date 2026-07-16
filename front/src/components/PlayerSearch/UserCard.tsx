fa723d16ff9c7d2e020d9e0dcb2fcdcb7bf3ba69
FutboLink <linkfutbo@gmail.com>
2026-07-16 12:57:53 +0200

    Update UserCard.tsx

--- front/src/components/PlayerSearch/UserCard.tsx
+++ front/src/components/PlayerSearch/UserCard.tsx
@@ -178,7 +178,7 @@ const UserCard: React.FC<UserCardProps> = ({
       <div className="flex flex-col items-center text-center">
         {/* Foto */}
         <div className="flex-shrink-0 relative">
-          <div className="w-20 h-20 rounded-full border-4 border-white shadow-md overflow-hidden mx-auto">
+          className="w-20 h-20 rounded-full overflow-hidden mx-auto bg-gray-100"
             <Image
               src={currentUser.imgUrl || getDefaultPlayerImage()}
               alt={`${currentUser.name} ${currentUser.lastname}`}

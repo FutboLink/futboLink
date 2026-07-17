4d678e4bd448dc330e2ec44520211c4e11305e0a
FutboLink <linkfutbo@gmail.com>
2026-07-17 18:37:16 +0200

    Update UserCard.tsx

--- front/src/components/PlayerSearch/UserCard.tsx
+++ front/src/components/PlayerSearch/UserCard.tsx
@@ -309,7 +309,6 @@ const UserCard: React.FC<UserCardProps> = ({
     showDropdown ? "opacity-100" : "opacity-0 group-hover:opacity-100"
   }`}
 >
-          <button
             <button
   className="flex items-center justify-center w-10 h-10 rounded-2xl border border-transparent text-gray-400 hover:text-gray-700 hover:bg-gray-50 hover:border-gray-200 transition-all duration-200"
 >

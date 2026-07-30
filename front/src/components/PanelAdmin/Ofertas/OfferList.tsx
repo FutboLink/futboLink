af34032af928f75b18aa515a88de2065def5d7a3
FutboLink <linkfutbo@gmail.com>
2026-07-30 15:39:16 +0200

    Update OfferList.tsx

--- front/src/components/PanelAdmin/Ofertas/OfferList.tsx
+++ front/src/components/PanelAdmin/Ofertas/OfferList.tsx
@@ -284,9 +284,12 @@ const sortedCountries = Object.entries(offersByCountry).sort(
     </option>
   ))}
 </select>
+
 </div>
 </div>
-      <div className="grid grid-cols-1 max-w-[100rem] mx-auto md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 py-4">
+</div>
+
+<div className="grid grid-cols-1 max-w-[100rem] mx-auto md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 py-4">
         {sortedOffers.length > 0 ? (
           sortedOffers.map((offer) => (
             <CardOffer

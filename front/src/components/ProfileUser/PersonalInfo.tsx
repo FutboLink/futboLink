4474756a0e6b83075ad5b61fac6782c5c6400f4a
FutboLink <linkfutbo@gmail.com>
2026-08-02 17:21:29 +0200

    Update PersonalInfo.tsx

--- front/src/components/ProfileUser/PersonalInfo.tsx
+++ front/src/components/ProfileUser/PersonalInfo.tsx
@@ -378,14 +378,11 @@ const PersonalInfo: React.FC<PersonalInfoProps> = ({ onProfileChange }) => {
 </div>
 
 {loading ? (
-  <p>{getText("Cargando los datos...", "loadingData")}</p>
-) : error ? (
+<p>{getText("Cargando los datos...", "loadingData")}</p>
+  ) : error ? (
   <p className="text-red-600">{error}</p>
-) : activeSection === "profile" ? (
-        <p>{getText("Cargando los datos...", "loadingData")}</p>
-      ) : error ? (
-        <p className="text-red-600">{error}</p>
-      ) : (
+  ) :
+  
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 mt-3">
           {/* Imagen de perfil (URL) */}
           <div

43c42d23f5708ebcfa631ded78413ae8ab9cc38c
FutboLink <linkfutbo@gmail.com>
2026-08-05 17:54:14 +0200

    Update ApplicantCard.tsx

--- front/src/components/Applications/ApplicantCard.tsx
+++ front/src/components/Applications/ApplicantCard.tsx
@@ -237,7 +237,7 @@ const ApplicantCard: React.FC<UserCardProps> = ({
         </div>
 
         {/* Información */}
-<div className="flex-1 grid grid-cols-[3.5fr_220px_170px_300px] items-center gap-4">
+<div className="flex-1 grid grid-cols-[3.5fr_220px_170px_170px] items-center gap-4">
 
 {/* Candidato */}
 <div>

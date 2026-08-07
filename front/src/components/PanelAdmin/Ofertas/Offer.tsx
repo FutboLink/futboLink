a7ac1afb00145aa2166ce1d77549098ed1f54802
FutboLink <linkfutbo@gmail.com>
2026-08-07 14:28:58 +0200

    Update Offer.tsx

--- front/src/components/PanelAdmin/Ofertas/Offer.tsx
+++ front/src/components/PanelAdmin/Ofertas/Offer.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import OfferStats from './OfferStats';
 import ManageOffers from './ManageOffers';
+import OfferAnalytics from "./OfferAnalytics";
 
 export default function Offer() {
   // Estado para controlar la pestaña activa
@@ -12,29 +13,52 @@ export default function Offer() {
     setActiveTab(tab);
   };
 
-  return (
-    <div className="p-4">
-      {/* Pestañas */}
-      <div className="flex space-x-4 mb-6">
-        <button
-          onClick={() => handleTabChange("stats")}
-          className={`px-4 py-2 ${activeTab === "stats" ? "bg-green-700 text-white" : "bg-gray-200 text-gray-700"} rounded`}
-        >
-          Datos de Ofertas
-        </button>
-        <button
-          onClick={() => handleTabChange("manage")}
-          className={`px-4 py-2 ${activeTab === "manage" ? "bg-green-700 text-white" : "bg-gray-200 text-gray-700"} rounded`}
-        >
-          Gestionar Ofertas
-        </button>
-      </div>
+return (
+  <div>
 
-      {/* Contenido según la pestaña activa */}
-      <div>
-        {activeTab === "stats" && <OfferStats />}
-        {activeTab === "manage" && <ManageOffers />}
-      </div>
+    {/* Pestañas */}
+    <div className="flex gap-4 mb-6">
+
+      <button
+        onClick={() => handleTabChange("stats")}
+        className={`px-4 py-2 ${
+          activeTab === "stats"
+            ? "bg-green-700 text-white"
+            : "bg-gray-200 text-gray-700"
+        } rounded`}
+      >
+        Datos de Ofertas
+      </button>
+
+      <button
+        onClick={() => handleTabChange("manage")}
+        className={`px-4 py-2 ${
+          activeTab === "manage"
+            ? "bg-green-700 text-white"
+            : "bg-gray-200 text-gray-700"
+        } rounded`}
+      >
+        Gestionar Ofertas
+      </button>
+
+      <button
+        onClick={() => handleTabChange("analytics")}
+        className={`px-4 py-2 ${
+          activeTab === "analytics"
+            ? "bg-green-700 text-white"
+            : "bg-gray-200 text-gray-700"
+        } rounded`}
+      >
+        Analytics
+      </button>
     </div>
-  );
-}
+
+    {/* Contenido según la pestaña activa */}
+    
+    <div>
+      {activeTab === "stats" && <OfferStats />}
+      {activeTab === "manage" && <ManageOffers />}
+      {activeTab === "analytics" && <OfferAnalytics />}
+    </div>
+  </div>
+);
